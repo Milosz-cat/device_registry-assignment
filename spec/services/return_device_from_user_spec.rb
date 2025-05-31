6b4c4e9d054cf03c2bd3a1f424838e646ee0307d
Milosz-cat <miloszbochenek20@gmail.com>
2025-05-31 12:47:29 +0200

    Fix: Changing import to correct one

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rspec'
+require 'rails_helper' # not 'rspec', becuase in Rails spec files, it is recommended to require 'rails_helper', not 'spec_helper' or 'rspec'. The 'rails_helper' file includes the 'spec_helper' file and also sets up the Rails environment, which is necessary for running tests that interact with the Rails framework
 
 RSpec.describe ReturnDeviceFromUser do
   # TODO: Implement the tests for ReturnDeviceFromUser
