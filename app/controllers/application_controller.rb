32da321451e112b3f40ec91a6efe35c6020a2c4d
Milosz-cat <miloszbochenek20@gmail.com>
2025-06-01 10:43:00 +0200

    fix(test failure): simplify user auth check and add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::API
   def authenticate_user!
     api_key = ApiKey.find_by(token: request.session[:token])
     @current_user = api_key&.bearer
-    head :unauthorized unless @current_user if @current_user.nil?
+    head :unauthorized unless @current_user # previous "head :unauthorized unless @current_user if @current_user.nil? " is weird, we check the same thing (@current_user) twice - DRY.
+  end
+
+  def current_user #standard convention in Rails
+    @current_user
   end
 end
