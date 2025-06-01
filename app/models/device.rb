2001ed6dbc3373642690359d5cff032ec0f5767a
Milosz-cat <miloszbochenek20@gmail.com>
2025-06-01 10:34:41 +0200

    fix(test failure): NoMethodError: undefined method 'devices' for User, add user-device associations

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,4 @@
 class Device < ApplicationRecord
+  belongs_to :owner, class_name: 'User', optional: true
+  belongs_to :previous_owner, class_name: 'User', optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
   has_secure_password
+
+  has_many :devices, class_name: 'Device', foreign_key: :owner_id, dependent: :nullify
+  has_many :previously_owned_devices, class_name: 'Device', foreign_key: :previous_owner_id, dependent: :nullify
 end
