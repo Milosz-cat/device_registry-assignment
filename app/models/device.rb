559492b2caf30f9d80d41ce440968df6474d3b46
Milosz-cat <miloszbochenek20@gmail.com>
2025-06-01 13:54:55 +0200

    feat(Device): add association with device_ownerships and historical_owners

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,12 @@
 class Device < ApplicationRecord
   belongs_to :owner, class_name: 'User', optional: true
-  belongs_to :previous_owner, class_name: 'User', optional: true
+
+  has_many :device_ownerships, dependent: :destroy
+  has_many :historical_owners, through: :device_ownerships, source: :user
 
   validates :serial_number, presence: true, uniqueness: true
+
+  def has_been_owned_by?(user)
+    device_ownerships.where(user: user).exists?
+  end
 end
