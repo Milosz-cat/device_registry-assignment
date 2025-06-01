5cae2cd8ce35bfa3ab901dd9b0d24d4556f4b135
Milosz-cat <miloszbochenek20@gmail.com>
2025-06-01 19:51:44 +0200

    feat(controller): implement #assigned to return current_user's assigned devices as JSON

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,5 +1,5 @@
 class DevicesController < ApplicationController
-  before_action :authenticate_user!, only: %i[assign unassign]
+  before_action :authenticate_user!, only: %i[assign unassign assigned]
 
   def assign
     AssignDeviceToUser.new(
@@ -13,17 +13,22 @@ class DevicesController < ApplicationController
     render json: { error: 'Unauthorized' }, status: :unprocessable_entity
   end
 
-def unassign
-  ReturnDeviceFromUser.new(
-    requesting_user: current_user,
-    serial_number: params.dig(:device, :serial_number),
-    from_user: current_user.id
-  ).call
+  def unassign
+    ReturnDeviceFromUser.new(
+      requesting_user: current_user,
+      serial_number: params.dig(:device, :serial_number),
+      from_user: current_user.id
+    ).call
 
-  head :ok
-rescue StandardError
-  render json: { error: 'Could not unassign device' }, status: :unprocessable_entity
-end
+    head :ok
+  rescue StandardError
+    render json: { error: 'Could not unassign device' }, status: :unprocessable_entity
+  end
+
+  def assigned
+    devices = Device.where(owner_id: current_user.id)
+    render json: devices.as_json(only: [:id, :serial_number])
+  end
 
   private
 
