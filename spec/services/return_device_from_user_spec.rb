4ca245c66f13b9ad2058cafad92eb0dc08266174
Milosz-cat <miloszbochenek20@gmail.com>
2025-06-01 14:47:55 +0200

    test(service): tests setup for ReturnDeviceFromUser

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper' # not 'rspec', becuase in Rails spec files, it is recommended to require 'rails_helper', not 'spec_helper' or 'rspec'. The 'rails_helper' file includes the 'spec_helper' file and also sets up the Rails environment, which is necessary for running tests that interact with the Rails framework
 
 RSpec.describe ReturnDeviceFromUser do
-  # TODO: Implement the tests for ReturnDeviceFromUser
+  subject(:return_device) do
+    described_class.new(
+      requesting_user: requesting_user,
+      serial_number: serial_number,
+      from_user: from_user_id
+    ).call
+  end
+
+  let(:requesting_user) { create(:user) }
+  let(:serial_number) { '987654' }
 end
